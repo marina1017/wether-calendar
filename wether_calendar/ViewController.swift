eb4932c61edefcc13c2502c65289596cf368f14c
marina1017 <mari10172001@yahoo.co.jp>
2018-03-30 23:45:05 +0900

    全部あげた

---
+++ wether_calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wether_calendar
+//
+//  Created by 中川万莉奈 on 2018/03/30.
+//  Copyright © 2018年 中川万莉奈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
